9d0f4ada63082cfd36e2f1b356f4a3fff4cd461f
Alireza <alirezabagheri3461@gmail.com>
2022-07-22 20:45:06 +0430

    replace some code

--- all code.js
+++ all code.js
@@ -247,7 +247,7 @@ function send_1(){
         return;
     }
     
-    var final = start();
+    var final = formatchat('start');
     if(final == null){
         return null ;
     }
@@ -269,7 +269,7 @@ function send_2(){
     document.getElementById('time_p').innerHTML = now_time;
     document.getElementById('send_1').click();
 }
-// mode_us
+//reply
 function message_reply(value){
     var d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18,d19,d20,d21,d22,d23,d24,
 d25,d26,d27,d28,d29,d30,d31,d32,d33,d34,d35,d36,d37,d38,d39,d40;
@@ -1940,6 +1940,294 @@ function search(value , sh){
     }
     return false;
 }
+// mode_us
+function mode_us(name,value,MY_BOT,Last_MS,Last_MS_1){
+
+    var person = document.getElementById('users_person');
+    var person_veri = person.childElementCount;
+    if(person_veri !== '0'){
+        var persons = person.childNodes;
+        var count = persons.length;
+        for(var i=0;i < count;i++){
+            var name_person = persons[i].getAttribute('name');
+            if(name_person == name){
+                var name_us = persons[i].childNodes[0].innerHTML;
+                var type_us = persons[i].childNodes[1].innerHTML;
+                var name_base = name;
+                return know_preson(name_us,value,MY_BOT,Last_MS,Last_MS_1,type_us,name_base);
+            }
+        }
+        return unknow_person(name,value,MY_BOT,Last_MS,Last_MS_1);
+    }
+    return unknow_person(name,value,MY_BOT,Last_MS,Last_MS_1);
+}
+
+function know_preson(name,value,MY_BOT,Last_MS,Last_MS_1,type_us,name_base){
+    if(value == 'منو از حافظت پاک کن'){
+
+         var person = document.getElementById('users_person');
+        var person_veri = person.childElementCount;
+        if(person_veri !== '0'){
+            var persons = person.childNodes;
+            var count = persons.length;
+            for(var i=0;i < count;i++){
+                var name_person = persons[i].getAttribute('name');
+                if(name_person == name_base){
+                    person.removeChild(person.childNodes[i]);
+                    var ANS = short_ANS('unknow')+'\n\n';
+                    ANS += 'اسمم بهار دخترم ⓵\nاسمم مهدی پسرم ⓶';
+                    formatchat('validate',ANS);
+                }
+            }
+           
+        }
+        
+    }
+    if(value == 'حالت هوشمند خاموش'){
+        document.getElementById('mood').innerHTML = 'off';
+         var ANS = short_ANS("off_mood");
+         formatchat('state',ANS);
+    }
+    if(value == 'حالت هوشمند روشن'){
+        var OFFON =document.getElementById('mood').innerHTML = 'on';
+         var ANS = short_ANS("on_mood");
+         formatchat('state',ANS);
+    }
+   var ans = search(value , 'حالت ارام');
+   if(ans){
+        var t = 't';
+        var pos_1 = value.indexOf(t);
+        var pos_2 = value.lastIndexOf(t);
+        var time_n = value.substring((pos_1+1),pos_2);
+        document.getElementById('time').innerHTML = time_n;
+        var ANS = short_ANS("time");
+        formatchat('state',ANS);
+   }
+    var ans = search(value , 'ربات');
+    var ans_1 = search(value , 'بات');
+    var ans_2 = search(value , 'گاگولی');
+   if(ans || ans_1 || ans_2){
+        var ANS =  short_ANS(type_us)+'\n';
+        ANS +=  message_reply(value);
+        return ANS;
+   }
+   var ans = search(value , 'راهنما');
+   if(ans){
+           var ANS = Help_GOGOLI();
+           return '______ #Help' + '\n'+ANS;
+   }
+   
+   ans = search(value , 'فال' );
+   if(ans){
+           var FAl = Game_FAl();
+           var ANS = name +" "+ 'جونم' + ' '+"اینم فالی که برات گرفتم"+" \n\n "+FAl;
+           formatchat('Omen',ANS);
+   }   
+   ans = search(value , 'اعتراف' );
+   if(ans){
+           var ETR = Game_ETR();
+           var ANS = name +" "+ 'جونم' + ' '+"اینم ی اعتراف از ی بنده خدا"+" \n\n "+ETR;
+           formatchat('Confess',ANS);
+   }
+   ans = search(value , 'چالش' );
+   if(ans){
+           var CHL = Game_CHL();
+           var ANS = name+" "+"جونم\n\n"+CHL;
+           formatchat('Challenge',ANS);
+   }
+   
+   
+   ans = search(value , 'تاریخ');
+   if(ans){
+        const egDateTime = new Intl.DateTimeFormat("eg", {
+            weekday: "long",
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            timeZoneName: "short"
+          }).format;
+          const now = Date.now();
+        var typeeg = egDateTime(now);
+        const faDateTime = new Intl.DateTimeFormat("fa", {
+            weekday: "long",
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            timeZoneName: "short"
+          }).format;
+          const now1 = Date.now();
+        var typefa = faDateTime(now1);
+        var sh = 'تاریخ شمسی 🌝';
+        var ml = 'Gregorian date 🌚';
+        // const faDate = new Intl.DateTimeFormat("fa", {
+        // weekday: "long",
+        // year: "numeric",
+        // month: "long",
+        // day: "numeric"
+        // }).format(date);
+
+        var ANS = sh + '\n' + '\n'+ typefa + '\n' + '\n'+ '______' + '\n' + ml + '\n'  + '\n'+ typeeg;
+        
+        formatchat('DATE',ANS);
+    }    
+
+   if(value == 'امار' ){
+        var users = document.getElementById('users');
+        var users_length = users.childNodes.length;
+        var ANS = '';
+        ANS += 'تعداد پیام هر یک از اعضای فعال گپ ||'+'\n'+'\n';
+        var all_ms;
+        all_ms = 0;
+        for(var i = 0;i < users_length; i++){
+            var ref = users.childNodes[i].hasChildNodes();
+            if(ref){
+                var name_ms = users.childNodes[i].getAttribute('name');
+                var person = document.getElementById('users_person');
+                var person_veri = person.childElementCount;
+                if(person_veri !== '0'){
+                    var persons = person.childNodes;
+                    var count = persons.length;
+                    for(var c=0;c < count;c++){
+                        var name_person = persons[c].getAttribute('name');
+                        if(name_person == name_ms){
+                            var name_us = persons[c].childNodes[0].innerHTML;
+                            var type_us = persons[c].childNodes[1].innerHTML;
+                            if(type_us == 'دخترم'){
+                                name_ms = short_ANS('girl_pass')+' ';
+                            }
+                            if(type_us == 'پسرم'){
+                                name_ms = short_ANS('boy_pass')+' ';
+                            }
+                            name_ms += name_us;
+                        }
+                    }
+                   }
+                var ms_user_length = users.childNodes[i].childNodes.length;
+                all_ms += ms_user_length;
+                ANS += name_ms + '  :  '+ms_user_length+'\n';
+            }
+        }
+        ANS += '\n'+'⊶'+'تعداد افراد فعال گپ'+' : '+(users_length-1)+'\n';
+        ANS += '⊷'+'تعداد کل پیام :'+' '+all_ms;
+        formatchat('Statistics',ANS);
+   }      
+
+   ans = search(value , 'بگو');
+   if(ans){
+        var ANS = value.replace('بگو','');
+        ans = search(ANS, 'گوگولی');
+        if(ans){
+            ANS = ANS.replace('گوگولی','');
+            return ANS;
+        }
+        return ANS;
+   }   
+   
+var Reply = Last_MS.getElementsByClassName('reply-content')[0];
+var submit = Last_MS.contains(Reply);
+if(submit ){
+        var US_ME =Last_MS.getElementsByClassName('peer-title')[1].innerHTML;
+        if(US_ME == MY_BOT){
+            var ANS =  message_reply(value);
+            return ANS;
+        }else{
+            ans = search(value , 'گوگولی');
+            if(ans){
+                ans = search(value , 'اسم');
+                if(ans){
+                    var ANS = Last_MS.getElementsByClassName('peer-title')[1].innerHTML;
+                    return ANS;
+                }
+            }
+            // ans = search(value , 'گوگولی');
+            // if(ans){
+            //     ans = search(value , 'امارش');
+            //     if(ans){
+            //         var users = document.getElementById('users');
+            //         var users_length = users.childNodes.length;
+            //         var ANS = '__ #Statistics'+'\n'+'\n'+'AL2P2LA'+'\n';
+            //         ANS += ' پیام های'+'  '+US_ME+'\n\n';
+            //         var all_ms;
+            //         all_ms = 0;
+            //         for(var i = 0;i < users_length; i++){
+            //             var ref = users.childNodes[i].hasChildNodes();
+            //             if(ref){
+            //                 var name_ms = users.childNodes[i].getAttribute('name');
+            //                 if(name_ms == US_ME){
+            //                     var ms_user_length = users.childNodes[i].childNodes.length;
+            //                     all_ms += ms_user_length;
+            //                     for(var c = 0;c< ms_user_length;c++){
+            //                     var ms_s = users.childNodes[i].childNodes[c].innerHTML;
+            //                         ANS += c +' : '+ms_s+'\n';
+            //                     }
+            //                 }
+                            
+            //             }
+            //         }
+            //         return ANS;
+            //     }
+            // }   
+            return false;
+        }
+    }
+    
+    var mood = document.getElementById('mood').innerHTML;
+    if(mood == 'on'){
+        var MS_1 = Last_MS_1.getElementsByClassName('message')[0];
+            var MS_us_1 = Last_MS_1.contains(MS_1);
+            if(MS_us_1){
+            var ls_n = Last_MS_1.getElementsByClassName('peer-title')[0].innerHTML;
+            if(ls_n == MY_BOT){
+                var ANS =  message_reply(value);
+                return ANS;
+            }
+        }
+    }
+
+    ans = search(value , 'گوگولی');
+    if(ans){
+        var ANS =  message_reply(value);
+        return ANS;
+    }
+    
+
+    var ANS = message(value , name);
+    return ANS;
+}
+
+function unknow_person(name,value,MY_BOT,Last_MS,Last_MS_1){
+    if(name == ''){
+        return false;
+    }
+    ans = search(value , 'اسمم');
+    if(ans){
+        var ans1 = search(value , 'دخترم');
+        if(ans1){
+            var nikname = value.replace('اسمم','');
+            nikname = nikname.replace('دخترم','');
+            nikname = nikname.trim();
+            document.getElementById('users_person').innerHTML += '<div class="nameBot_user" name="'+name+'"><p>'+nikname+'</p><p>دخترم</p></div>';
+            var ANS =  "پس خانومی اسمت"+" "+nikname+" "+'هست'+'\n'+'ثبت شد.';
+            return ANS;
+        }
+        var ans2 = search(value , 'پسرم');
+        if(ans2){
+            var nikname = value.replace('اسمم','');
+            nikname = nikname.replace('پسرم','');
+            nikname = nikname.trim();
+            document.getElementById('users_person').innerHTML += '<div class="nameBot_user" name="'+name+'"><p>'+nikname+'</p><p>پسرم</p></div>';
+            var ANS =  "پس اقایی اسمت"+" "+nikname+" "+ 'هست'+'\n'+'ثبت شد.';
+            return ANS;
+        }
+    }
+    
+   document.getElementById('users_person').innerHTML += '<div class="nameBot_user" name="'+name+'"><p>'+name+'</p><p></p></div>';
+     
+}
 // mode_imp
 function mode_imp(name,value,MY_BOT,Last_MS,Last_MS_1){
     var ans = search(value , 'شروع');
@@ -2638,14 +2926,6 @@ function Game_CHL(){
     return bank[num];
    }
 // formatcht
-formatchat('state',value);
-formatchat('Omen',value);
-formatchat('Confess',value);
-formatchat('Challenge',value);
-formatchat('DATE',value);
-formatchat('Statistics',value);
-formatchat('validate',value);
-formatchat('start');
 function formatchat(type,value){
     if(type == 'state'){
         var header = 'Ⲷ #وضعیت'+'\n\n';
--- formatchat.js
+++ formatchat.js
@@ -1,49 +1,51 @@
-function state(value){
-    var header = 'Ⲷ #وضعیت'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ ⚠ ━━━━┅─';
-    return (header+main+footer);
-}
-function Omen(value){
-    var header = 'Ⲷ #فال'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ ⚛ ━━━━┅─';
-    return (header+main+footer);
-}
-function Confess(value){
-    var header = 'Ⲷ #اعتراف'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ ⛔ ━━━━┅─';
-    return (header+main+footer);
-}
-function Challenge(value){
-    var header = 'Ⲷ #چالش'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ ☢ ━━━━┅─';
-    return (header+main+footer);
-}
-function DATE(value){
-    var header = 'Ⲷ #تاریخ'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ ⏰ ━━━━┅─';
-    return (header+main+footer);
-}
-function Statistics(value){
-    var header = 'Ⲷ #آمار'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ ␟ ━━━━┅─';
-    return (header+main+footer);
-}
-function validate(value){
-    var header = 'Ⲷ #احراز_هویت'+'\n\n';
-    var main = value+'\n';
-    var footer = '─┅━━━━ೊ ━━━━┅─';
-    return (header+main+footer);
-}
-function start(){
-    var header = 'ربات با موفقیت بر  روی گپ نصب شد'+'\n';
-    var main = "گوگولی هستم ֍_֍"+'\n\n';
-    main += "راهنما"+'\n';
-    var footer = '─┅━━━━ ␀ ━━━━┅─';
-    return (header+main+footer);
+function formatchat(type,value){
+    if(type == 'state'){
+        var header = 'Ⲷ #وضعیت'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ ⚠ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'Omen'){
+        var header = 'Ⲷ #فال'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ ⚛ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'Confess'){
+        var header = 'Ⲷ #اعتراف'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ ⛔ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'Challenge'){
+        var header = 'Ⲷ #چالش'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ ☢ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'DATE'){
+        var header = 'Ⲷ #تاریخ'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ ⏰ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'Statistics'){
+        var header = 'Ⲷ #آمار'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ ␟ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'validate'){
+        var header = 'Ⲷ #احراز_هویت'+'\n\n';
+        var main = value+'\n';
+        var footer = '─┅━━━━ೊ ━━━━┅─';
+        return (header+main+footer);
+    }
+    if(type == 'start'){
+        var header = 'ربات با موفقیت بر  روی گپ نصب شد'+'\n';
+        var main = "گوگولی هستم ֍_֍"+'\n\n';
+        main += "راهنما"+'\n';
+        var footer = '─┅━━━━ ␀ ━━━━┅─';
+        return (header+main+footer);
+    }
 }
